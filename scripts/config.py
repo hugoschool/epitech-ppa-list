249b07ea5161523e7ce5d06bb5f9a2fa607d0951
Hugo <hugo@hugoarnal.com>
2025-09-25 09:12:17 +0200

    chore: update config

--- scripts/config.py
+++ scripts/config.py
@@ -1,21 +1,5 @@
 DOWNLOAD_FOLDER = "packages/archives"
 
-PPA_CONFIG = [
-    {
-        "ppa": "epitech/ppa",
-        "packages": [
-            "epitech-cpool",
-            "epitech-criterion",
-            "epitech-dump",
-            "epitech-emacs",
-            "epitech-full",
-            "epitech-premsc",
-            "epitech-tek1",
-            "epitech-tek2",
-            "epitech-vim",
-            "epitech-web",
-        ],
-    }
-]
+PPA_CONFIG = ["epitech/ppa"]
 
 FINAL_FILE = "packages/packages.json"
