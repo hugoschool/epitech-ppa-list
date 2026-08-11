9a812d3cdb1fa99896419482e8ee5e191ec54170
Hugo ARNAL <hugo@hugoarnal.com>
2026-08-12 01:43:35 +0200

    ref: make fetching script only get from a singular PPA
    This script was made in case there was multiple PPAs to be fetched from.
    However I don't think Epitech, or even myself, will ever use more than one.

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,9 @@
 DOWNLOAD_FOLDER = "packages/archives"
 
-PPA_CONFIG = ["epitech/ppa"]
+PPA_TARGET = {
+    "author": "epitech",
+    # See https://api.launchpad.net/devel.html#archive
+    "archive_name": "ppa",
+}
 
 FINAL_FILE = "packages/packages.js"
